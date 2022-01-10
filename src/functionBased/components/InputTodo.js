ab603bc5370f2f816acd1efdd260b658fb74a33d
Amkam <amaduskamara@gmail.com>
2022-01-10 20:37:18 +0000

    Stored files in classBased and functionBased directories

---
+++ src/functionBased/components/InputTodo.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class InputTodo extends Component {
+  state = {
+    title: "",
+  };
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+ 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: "",
+      });
+    } else {
+      alert("Please add a todo item");
+    }
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          className="input-text"
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
+        />
+        <button className="input-submit">Submit</button>
+      </form>
+    );
+  }
+}
+export default InputTodo;

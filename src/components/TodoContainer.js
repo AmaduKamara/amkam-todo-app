d4d2025d8d2dccf94674bc70e76a93ea9b36023e
Amkam <amaduskamara@gmail.com>
2022-01-10 20:23:41 +0000

    Completed using the LifeCycle methods and class based components

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -7,23 +7,7 @@ import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
+    todos: [],
   };
   handleChange = (id) => {
     this.setState((prevState) => {
@@ -60,15 +44,30 @@ class TodoContainer extends React.Component {
     });
   };
   setUpdate = (updatedTitle, id) => {
-   this.setState({
-     todos: this.state.todos.map((todo) => {
-       if (todo.id === id) {
-         todo.title = updatedTitle;
-       }
-       return todo;
-     }),
-   });
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      }),
+    });
   };
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem("todos", temp);
+    }
+  }
+  componentDidMount() {
+    const temp = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
+  }
   render() {
     return (
       <div className="container">

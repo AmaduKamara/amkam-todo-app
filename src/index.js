9233aa7f1146add0e03c5b71862dab87158057f4
Amkam <amaduskamara@gmail.com>
2022-01-11 15:35:19 +0000

    Add routing and hamburger menu in the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 //component file
 import TodoContainer from "./functionBased/components/TodoContainer";
 
@@ -8,7 +9,9 @@ import "./functionBased/App.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );

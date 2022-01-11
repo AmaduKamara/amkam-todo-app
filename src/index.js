8a9e950874596ce4e25b282f5ddc88720f0c3f65
Amkam <amaduskamara@gmail.com>
2022-01-11 16:10:41 +0000

    Add 404.html and configured home route in index.html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 //component file
-import TodoContainer from "./functionBased/components/TodoContainer";
+import TodoContainer from './functionBased/components/TodoContainer';
 
 //stylesheet
-import "./functionBased/App.css";
+import './functionBased/App.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
